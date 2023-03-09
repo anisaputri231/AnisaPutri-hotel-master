595a942d3bb3fd074600df3b6bcb9d76319d63ac
nasir14 <nasir@smkn1rongga.sch.id>
2023-03-09 16:30:18 +0700

    jskdjk

--- hotel.sql
+++ hotel.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 07, 2023 at 12:18 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Mar 09, 2023 at 10:29 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `fasilitas_hotel` (
   `nama_fasilitashotel` varchar(255) NOT NULL,
   `keterangan` text NOT NULL,
   `gambar` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `fasilitas_hotel`
@@ -56,7 +56,7 @@ CREATE TABLE `fasilitas_kamar` (
   `fasilitaskamar_id` int(11) NOT NULL,
   `kamar_id` int(11) NOT NULL,
   `fasilitas` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `fasilitas_kamar`
@@ -82,14 +82,15 @@ CREATE TABLE `guest` (
   `username` varchar(100) NOT NULL,
   `password` text NOT NULL,
   `tlp` varchar(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `guest`
 --
 
 INSERT INTO `guest` (`id`, `name`, `username`, `password`, `tlp`) VALUES
-(1, 'a', 'a', '0cc175b9c0f1b6a831c399e269772661', '4');
+(1, 'a', 'a', '0cc175b9c0f1b6a831c399e269772661', '4'),
+(2, 'b', 'b', '92eb5ffee6ae2fec3ad71c777531578f', '6');
 
 -- --------------------------------------------------------
 
@@ -103,16 +104,16 @@ CREATE TABLE `kamar` (
   `harga` int(100) NOT NULL,
   `gambar` text NOT NULL,
   `status` enum('checkin','checkout') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `kamar`
 --
 
 INSERT INTO `kamar` (`id`, `tipe_kamar`, `harga`, `gambar`, `status`) VALUES
-(1, 'Standard room', 250, 'Alt-Text-1.jpg', 'checkout'),
+(1, 'Standard room', 250, '', ''),
 (2, 'Superior room', 300, 'Alt-Text-2.jpg', 'checkin'),
-(3, 'Deluxe room', 300, 'Alt-Text-3.jpg', 'checkout'),
+(3, 'Deluxe room', 300, '', 'checkin'),
 (4, 'Twin room', 150, 'Alt-Text-4.jpg', 'checkin'),
 (5, 'Single room', 750, 'Alt-Text-5.jpg', 'checkout'),
 (6, 'Double room', 500, 'Alt-Text-6.jpg', 'checkin'),
@@ -131,7 +132,7 @@ INSERT INTO `kamar` (`id`, `tipe_kamar`, `harga`, `gambar`, `status`) VALUES
 
 CREATE TABLE `reservasi` (
   `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `no_hp` int(20) NOT NULL,
   `kamar_id` int(11) NOT NULL,
@@ -139,19 +140,15 @@ CREATE TABLE `reservasi` (
   `checkout` date NOT NULL,
   `jumlah` int(11) NOT NULL,
   `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `reservasi`
 --
 
-INSERT INTO `reservasi` (`id`, `nama`, `email`, `no_hp`, `kamar_id`, `checkin`, `checkout`, `jumlah`, `status`) VALUES
-(24, 'rika', 'rika@gmail.com', 2147483647, 5, '2023-02-05', '2023-02-28', 276000, 'checkin'),
-(25, 'tasya', 'tasya@gmail.com', 2111345, 6, '2023-02-02', '2023-02-05', 600, 'proses'),
-(26, 'tasya', 'tasya@gmail.com', 2111345, 6, '2023-02-02', '2023-02-05', 600, 'proses'),
-(27, 'risma', 'risma@gmail.com', 444, 5, '2023-02-07', '2023-02-08', 750, 'proses'),
-(29, 'risma', 'anisaputri@student.smkn1rongga.sch.id', 444, 4, '2023-02-21', '2023-02-23', 300, 'proses'),
-(30, 'risma', 'risma@gmail.com', 444, 10, '2023-02-21', '2023-02-28', 6650, 'proses');
+INSERT INTO `reservasi` (`id`, `user_id`, `email`, `no_hp`, `kamar_id`, `checkin`, `checkout`, `jumlah`, `status`) VALUES
+(36, 2, 'ujang@gmail.com', 111, 1, '2023-03-11', '2023-03-25', 3500, 'checkin'),
+(37, 2, 'ujang@gmail.com', 111, 1, '2023-03-11', '2023-03-25', 3500, 'proses');
 
 -- --------------------------------------------------------
 
@@ -165,8 +162,8 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `no_telp` int(11) NOT NULL,
-  `role` enum('admin','resevsionis','user') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role` enum('admin','resevsionis') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
@@ -175,7 +172,7 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `name`, `username`, `password`, `no_telp`, `role`) VALUES
 (1, 'adminha', 'adminmaster', '21232f297a57a5a743894a0e4a801fc3', 83112, 'admin'),
 (2, 'putrirahayu\r\n', 'anisa', '4093fed663717c843bea100d17fb67c8', 83221, 'resevsionis'),
-(10, 'nis', 'a', '0cc175b9c0f1b6a831c399e269772661', 11111, 'user');
+(10, 'nis', 'a', '0cc175b9c0f1b6a831c399e269772661', 11111, '');
 
 --
 -- Indexes for dumped tables
@@ -237,7 +234,7 @@ ALTER TABLE `fasilitas_kamar`
 -- AUTO_INCREMENT for table `guest`
 --
 ALTER TABLE `guest`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `kamar`
@@ -249,7 +246,7 @@ ALTER TABLE `kamar`
 -- AUTO_INCREMENT for table `reservasi`
 --
 ALTER TABLE `reservasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `user`
